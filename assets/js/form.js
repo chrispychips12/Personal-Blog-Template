72f07273e18e8d8ae3b5402d777c311ae2c5f559
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-06-29 12:06:36 +1000

    form.js begin

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,10 @@
 // TODO: Create a variable that selects the form element
+const blogForm = document.getElementById('blogForm');
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
-
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    
 let redirectURL = '';
 
 const redirectPage = function (url) {
